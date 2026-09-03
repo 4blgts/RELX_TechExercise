79abe4af7293aa3f45fd9d26427a3843cbf318d3
Paul Balagtas <4blgts@gmail.com>
2026-09-04 02:36:20 +0800

    Commiting changes for the QA challenge

--- playwright-ts-framework/src/pages/base.page.ts
+++ playwright-ts-framework/src/pages/base.page.ts
@@ -12,44 +12,26 @@ export default class BasePage {
     await locator.click();
   }
 
-  public get hamburgerMenu(): Locator {
-    return this.page.getByRole('button', { name: 'Open menu', exact: true });
-    }
-
-  public menuItem(menuItemName: string ): Locator {
-    return this.page.getByRole('link', { name: menuItemName, exact: true });
-    }
-
-  public get accountLink(): Locator {
-    return this.page.getByRole('link', { name: 'Account', exact: true }).first();
-    }
-
-  public get signUpLink(): Locator {
-    return this.page.getByRole('link', { name: 'Sign up', exact: true }).first();
-    }
-
   public get storefrontHeader(): Locator {
-    return this.page.getByRole('heading', { name: 'Spree Storefront' });
-  }
+    return this.page.getByRole('img', { name: 'Plaza San Juan Macías' }).first();
+    }
 
-  async clickHamburgerMenu(): Promise<void> {
-        await this.hamburgerMenu.waitFor({ state: 'visible' });
-        await this.hamburgerMenu.click();
+  public get myAccountLink(): Locator {
+    return this.page.getByRole('link', { name: 'Mi Cuenta' }).first();
+    }
+
+  public get catalogLink(): Locator {
+    return this.page.getByRole('link', { name: 'Catálogo' });
+    }
+
+  public get categoriesLink(): Locator {
+    return this.page.getByRole('link', { name: 'Categorías' });
+    }
+
+  async clickMyAccountLink(): Promise<void> {
+        await this.myAccountLink.waitFor({ state: 'visible' });
+        await this.myAccountLink.click();
      }
 
-  async navigateToMenuItem(menuItemName: string): Promise<void> {
-        await this.clickHamburgerMenu();
-        await this.menuItem(menuItemName).waitFor({ state: 'visible' });
-        await this.menuItem(menuItemName).click();
-  }
-
-  async clickAccountLink(): Promise<void> {
-        await this.accountLink.waitFor({ state: 'visible' });
-        await this.accountLink.click();
-     }
-
-  async clickSignUpLink(): Promise<void> {
-        await this.signUpLink.waitFor({ state: 'visible' });
-        await this.signUpLink.click();
-     }  
+ 
 }
--- playwright-ts-framework/tests/demoHappyPath.spec.ts
+++ playwright-ts-framework/tests/demoHappyPath.spec.ts
@@ -1,99 +1,30 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
 import BasePage from '../src/pages/base.page';
-import CreateUserPage from '../src/pages/createUser.page';
 import testData from '../src/data/demo.json';
-import AccountOverviewPage from '../src/pages/accountOverview.page';
-import ProductsPage from '../src/pages/products.page';
-import ShoppingCartPage from '../src/pages/shoppingCart.page';
-import CheckoutPage from '../src/pages/checkout.page';
+import MyAccountPage from '../src/pages/myAccount.page';
 
-test('Happy Path - Create new user and complete product checkout flow', async ({ page }) => {
+test('Happy Path - Select Product from category and checkout', async ({ page }) => {
   
     //Page instantiation
     const basePage = new BasePage(page);
-    const createUserPage = new CreateUserPage(page);
-    const accountOverviewPage = new AccountOverviewPage(page);
-    const productPage = new ProductsPage(page);
-    const shoppingCartPage = new ShoppingCartPage(page);
-    const checkoutPage = new CheckoutPage(page);
-
-    //Constants declaration
-    const simpleEmail = faker.internet.email();
-
+    const myAccountPage = new MyAccountPage(page);
+    
     await test.step('Navigate to base URL', async () => {
         await basePage.navigateTo(process.env.BASE_URL!);
         await expect(basePage.storefrontHeader).toBeVisible();
     });
 
-    await test.step('Click on Account link', async () => {
-        await basePage.clickAccountLink();
+    await test.step('Click on My Account link', async () => {
+        await basePage.clickMyAccountLink();
     });
 
-    await test.step('Click on Sign Up link', async () => {
-        await basePage.clickSignUpLink();
+    await test.step('Login with valid credentials', async () => {
+        await myAccountPage.Login(testData.customerLogin.username, testData.customerLogin.password);
     });
 
-    await test.step('Fill in the user details and create account', async () => {
-        await createUserPage.fillFirstName(testData.newUserDetails.firstName);
-        await createUserPage.fillLastName(testData.newUserDetails.lastName);
-        await createUserPage.fillEmail(simpleEmail);
-        await createUserPage.fillPassword(testData.newUserDetails.password);
-        await createUserPage.fillConfirmPassword(testData.newUserDetails.password);
-        await createUserPage.clickPrivacyPolicyCheckBox();
-        await createUserPage.clickCreateAccountButton();
-
+    await test.step('Navigate to Catalog and then Categories', async () => {
+        await basePage.clickElement(basePage.catalogLink);
+        await basePage.clickElement(basePage.categoriesLink);
     });
 
-    await test.step('Verify account creation and user was navigated to Account Overview page', async () => {
-        await expect(accountOverviewPage.accountEmail(simpleEmail)).toBeVisible();
-    });
-
-    await test.step('Sign Out and Sign In using the new account', async () => {
-        await accountOverviewPage.clickSignOutButton();
-
-        // Sign In using the new account
-        await accountOverviewPage.fillEmail(simpleEmail);
-        await accountOverviewPage.fillPassword(testData.newUserDetails.password);
-        await accountOverviewPage.clickSignInButton();
-        await expect(accountOverviewPage.accountEmail(simpleEmail)).toBeVisible();
-    });
-    
-    await test.step('Navigate to All Products page', async () => {
-        await basePage.navigateToMenuItem('All Products');
-        await expect(productPage.productsHeader).toBeVisible();
-    });
-
-    await test.step('Select a product and add to cart', async () => {
-        await productPage.selectProductandAddToCart(testData.productDetails.productName);
-        await expect(shoppingCartPage.viewCartLink).toBeVisible();
-    });
-
-    await test.step('Click on View Cart link and verify the product is in the shopping cart', async () => {
-        await shoppingCartPage.clickViewCartButton();
-        await expect(shoppingCartPage.shoppingCartHeader).toBeVisible();
-
-        //Verify if product was successfully added to the shopping cart
-        await expect(shoppingCartPage.productNameHeader(testData.productDetails.productName)).toBeVisible();
-    });
-
-    await test.step('Proceed to checkout', async() => {
-        await shoppingCartPage.clickCheckOutButton();
-        await expect(checkoutPage.shippingAddressHeader).toBeVisible();
-    });
-
-    await test.step('Populate checkout required fields and proceed with order', async() => {
-        await checkoutPage.fillShippingAddress(
-            testData.checkoutDetails.Address,
-            testData.checkoutDetails.City,
-            testData.checkoutDetails.State,
-            testData.checkoutDetails.ZipCode
-        );
-        await checkoutPage.clickPlaceOrderButton();
-      });
-
-    await test.step('Verify order confirmation page is displayed', async() => {
-        await checkoutPage.orderConfirmationHeaderBlock.waitFor({ state: 'attached', timeout: 15000 });
-        await expect(checkoutPage.orderConfirmationHeader).toBeVisible({ timeout: 30000 });
-    });
 })
